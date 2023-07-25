500a4024ffb0da50dd7c7aacfd0f06623eb72691
apurvasingla <131879390+apurvasingla@users.noreply.github.com>
2023-07-25 16:45:02 +0530

    Add files via upload

---
+++ organize.py
@@ -0,0 +1,6 @@
+import os
+import shutil
+from_dir = "C:/Users/Laptop/Downloads"
+to_dir = "C:/Users/Laptop/OneDrive/Desktop/downloaded images"
+list_of_files = os.listdir(from_dir)
+print (list_of_files)
